f56ea218db5de55b5dc3ff8672b308150f042d31
MasterHunter363 <oneitalo9@gmail.com>
2026-04-09 20:25:22 -0300

    terceirocommit

--- src/projetogrupo6/Main.java
+++ src/projetogrupo6/Main.java
@@ -6,7 +6,15 @@ public class Main {
  
     public static void main(String[] args) {
         
-        
+         ArrayList<Pessoa> lista = new ArrayList<>(); 
+
+ 
+
+Pessoa p = new Pessoa(nome, idade,cpf); 
+
+ 
+
+lista.add(p);
     }
  
 }
--- src/projetogrupo6/Pessoa.java
+++ src/projetogrupo6/Pessoa.java
@@ -1,9 +1,13 @@
 
 package projetogrupo6;
  
+
+import java.util.Scanner;
+
  
 
 public class Pessoa {
- 
+    Scanner s = new Scanner(System.in);
+   
 }
 
