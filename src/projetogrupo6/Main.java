16ebc7fc302feeb94867604f3169e145baeec919
Thiago Wesley Cunha <thiagotwc@gmail.com>
2026-04-09 21:29:07 -0300

    menu,getAndset

--- src/projetogrupo6/Main.java
+++ src/projetogrupo6/Main.java
@@ -6,15 +6,24 @@ public class Main {
  
     public static void main(String[] args) {
         
-         ArrayList<Pessoa> lista = new ArrayList<>(); 
+        System.out.println("||============== Cadastro =============||");
+        System.out.println("||                                     ||");
+        System.out.println("||              1 - Nome:              ||");
+        System.out.println("||              2 - Idade:             ||");
+        System.out.println("||              3 - CPF:               ||");
+        System.out.println("||=====================================||");
+        
+//         ArrayList<Pessoa> lista = new ArrayList<>(); 
+//
+// 
+//
+//Pessoa p = new Pessoa(nome, idade,cpf); 
+//
+// 
+//
+//lista.add(p);
 
- 
 
-Pessoa p = new Pessoa(nome, idade,cpf); 
-
- 
-
-lista.add(p);
     }
  
 }
--- src/projetogrupo6/Pessoa.java
+++ src/projetogrupo6/Pessoa.java
@@ -1,13 +1,40 @@
 
 package projetogrupo6;
- 
-
-import java.util.Scanner;
-
- 
 
 public class Pessoa {
-    Scanner s = new Scanner(System.in);
-   
+    
+    private String nome; 
+    private int idade;
+    private int CPF;
+
+    public Pessoa(String nome, int idade, int CPF) {
+        this.nome = nome;
+        this.idade = idade;
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
 }
 
