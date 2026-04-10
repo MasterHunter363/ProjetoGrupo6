0a6e166cc94f4b18dbdabcac8f7b0c06a686723e
Thiago Wesley Cunha <thiagotwc@gmail.com>
2026-04-09 21:07:29 -0300

    quarto commit

--- src/projetogrupo6/Main.java
+++ src/projetogrupo6/Main.java
@@ -1,20 +1,24 @@
 package projetogrupo6;
- 
- 
+
+import java.util.ArrayList;
+
 public class Main {
- 
- 
+
     public static void main(String[] args) {
-        
-         ArrayList<Pessoa> lista = new ArrayList<>(); 
 
- 
+//        ArrayList<Pessoa> lista = new ArrayList<>();
+//         
+//
+//Pessoa p = new Pessoa(nome, idade, CPF);
+//         
+//
+//lista.add(p);
 
-Pessoa p = new Pessoa(nome, idade,cpf); 
-
- 
-
-lista.add(p);
+        System.out.println("||============== Cadastro =============||");
+        System.out.println("||                                     ||");
+        System.out.println("||              1 - Nome:              ||");
+        System.out.println("||              2 - Idade:             ||");
+        System.out.println("||              3 - CPF:               ||");
+        System.out.println("||=====================================||");
     }
- 
 }
